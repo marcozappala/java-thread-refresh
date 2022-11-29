666c1a6b2899989325b1fe57f8672422d70a6b72
marcozappala <marco.m.zappala@gmail.com>
2022-11-29 17:24:15 +0100

    rename ThreadSimple

---
+++ src/main/java/tutorial/ThreadInterruption.java
@@ -0,0 +1,71 @@
+package tutorial;
+
+public class ThreadInterruption {
+
+    // Display a message, preceded by
+    // the name of the current thread
+    static void threadMessage(String message) {
+        String threadName =
+                Thread.currentThread().getName();
+        System.out.format("%s: %s%n",
+                threadName,
+                message);
+    }
+
+    private static class MessageLoop
+            implements Runnable {
+        public void run() {
+            String importantInfo[] = {
+                    "Mares eat oats",
+                    "Does eat oats",
+                    "Little lambs eat ivy",
+                    "A kid will eat ivy too"
+            };
+            try {
+                for (int i = 0;
+                     i < importantInfo.length;
+                     i++) {
+                    // Pause for 4 seconds
+                    Thread.sleep(4000);
+                    // Print a message
+                    threadMessage(importantInfo[i]);
+                }
+            } catch (InterruptedException e) {
+                threadMessage("I wasn't done!");
+            }
+        }
+    }
+
+    public static void main(String args[])
+            throws InterruptedException {
+
+        // Delay, in milliseconds before
+        // we interrupt MessageLoop
+        // thread (default 30 seconds).
+        long patience = 1000 * 5;
+
+        threadMessage("Starting MessageLoop thread");
+        long startTime = System.currentTimeMillis();
+        Thread t = new Thread(new MessageLoop());
+        t.start();
+
+        threadMessage("Waiting for MessageLoop thread to finish");
+        // loop until MessageLoop
+        // thread exits
+        while (t.isAlive()) {
+            threadMessage("Still waiting...");
+            // Wait maximum of 1 second
+            // for MessageLoop thread
+            // to finish.
+            t.join(1000); // i wait 1 second before checking if we we cannot wait anymore
+            if (((System.currentTimeMillis() - startTime) > patience)
+                    && t.isAlive()) {
+                threadMessage("Tired of waiting!");
+                t.interrupt();
+                // Shouldn't be long now
+                t.join(); // -- wait indefinitely
+            }
+        }
+        threadMessage("Finally!");
+    }
+}
